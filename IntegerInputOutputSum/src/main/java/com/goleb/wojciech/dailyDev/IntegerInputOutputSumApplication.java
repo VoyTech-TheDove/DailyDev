a054db92c6695cbc9d330220ed7119dd471317a9
Wojciech <goleb.wojciech@gmail.com>
2023-03-24 07:53:05 +0100

    Initial commit

---
+++ IntegerInputOutputSum/src/main/java/com/goleb/wojciech/dailyDev/IntegerInputOutputSumApplication.java
@@ -0,0 +1,13 @@
+package com.goleb.wojciech.dailyDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerInputOutputSumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegerInputOutputSumApplication.class, args);
+    }
+
+}
